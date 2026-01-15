0243fa0acc5e60a94bd2f8a224a7ef6871efd417
tamtam113 <tamtam113.tg@gmail.com>
2026-01-15 11:39:09 -0500

    hi

--- promise.mjs
+++ promise.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 async function getUser(id) {
   try {
     const dbName = await central(id);
@@ -35,41 +34,3 @@ async function getUser(id) {
     return Promise.reject(error);
   }
 }
-=======
-async function getUser(id) {
-  try {
-    const dbName = await central(id);
-
-    const [dbData, vaultData] = await Promise.all([
-      dbs[dbName](id),
-      vault(id)
-    ]);
-
-    return {
-      id: id,
-      name: vaultData.name,
-      username: dbData.username,
-      email: vaultData.email,
-      address: {
-        street: vaultData.address.street,
-        suite: vaultData.address.suite,
-        city: vaultData.address.city,
-        zipcode: vaultData.address.zipcode,
-        geo: {
-          lat: vaultData.address.geo.lat,
-          lng: vaultData.address.geo.lng
-        }
-      },
-      phone: vaultData.phone,
-      website: dbData.website,
-      company: {
-        name: dbData.company.name,
-        catchPhrase: dbData.company.catchPhrase,
-        bs: dbData.company.bs
-      }
-    };
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
->>>>>>> 7efb44ca763303bbd3add727886057d6869fbba3
